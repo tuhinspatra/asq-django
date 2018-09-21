97fff9b57853273ea5abdc8a061c61ea009a8e0a
jugtaram <jagatsamdary@gmail.com>
2018-09-21 09:37:14 +0530

    upvote downvote updated

--- asq_app/forms.py
+++ asq_app/forms.py
@@ -10,4 +10,5 @@ class AnsForm(ModelForm):
 	class Meta:
 		model=Answer
 		fields=['body']
+		
 
--- asq_app/views.py
+++ asq_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, render,redirect
 from django.urls import reverse
 from django.views import generic
 from .forms import AskForm ,AnsForm
-from .models import Answer, Question
+from .models import Answer, Question,UserVoteDetail
 
 
 class IndexView(generic.ListView):
@@ -18,9 +18,8 @@ class IndexView(generic.ListView):
 def detail(request,slug):
     qdata=Question.objects.get(slug=slug)
     if request.method == 'POST':
-
         ansform = AnsForm(request.POST)
-        if ansform.is_valid():
+        if ansform.is_valid() and request.POST['body']:
             instance = ansform.save(commit=False)
             instance.author = request.user
             instance.question = qdata
@@ -54,19 +53,40 @@ def ansForm(request):
             ansform = AnsForm()
         return render(request,'asq_app/ansform.html',{'ansform':ansform})
 
+
 def upvoter(request):
-	answer_id = request.GET.get('answer_id')
-	answer = Answer.objects.get(pk=answer_id)
-	answer.upvotes += 1;
-	answer.save()
-	qdata = {'votes':answer.upvotes}
-	return JsonResponse(qdata)
+    answer_id = request.GET.get('answer_id')
+    question_id = request.GET.get('question_id')
+    try:
+        answer = Answer.objects.get(pk=answer_id)
+        question = Question.objects.get(pk=question_id)
+        status = UserVoteDetail.objects.get(answer=answer_id,question=question_id,user=request.user)
+    except UserVoteDetail.DoesNotExist:
+        status=None             
+    if status != None:
+        qdata = {'vote':"already voted"}
+    else:
+        answer.upvotes += 1;
+        UserVoteDetail.objects.create(answer=answer_id,question=question_id,user=request.user)
+        answer.save()
+        qdata = {'votes':answer.upvotes}
+    return JsonResponse(qdata)
 
 def downvoter(request):
-	answer_id = request.GET.get('answer_id')
-	answer = Answer.objects.get(pk=answer_id)
-	answer.downvotes += 1;
-	answer.save()
-	ansdata = {'votes':answer.downvotes}
-	return JsonResponse(ansdata)	
+    answer_id = request.GET.get('answer_id')
+    question_id = request.GET.get('question_id')
+    try:
+        answer = Answer.objects.get(pk=answer_id)
+        question = Question.objects.get(pk=question_id)
+        status = UserVoteDetail.objects.get(answer=answer_id,question=question_id,user=request.user)
+    except UserVoteDetail.DoesNotExist:
+        status=None             
+    if status != None:
+        qdata = {'vote':"already voted"}
+    else:
+        answer.downvotes += 1;
+        UserVoteDetail.objects.create(answer=answer_id,question=question_id,user=request.user)
+        answer.save()
+        qdata = {'votes':answer.downsvotes}
+    return JsonResponse(qdata)	
 
